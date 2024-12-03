fd5b105c2bec634c3402cb3181a35f539eea6028
Kevin Britten <kevinjbritten@gmail.com>
2024-12-03 18:05:43 -0500

    add tests

--- src/main/java/ca/vanier/ScoreManager.java
+++ src/main/java/ca/vanier/ScoreManager.java
@@ -7,32 +7,34 @@ public class ScoreManager {
     public int getScores() {
         return scores;
     }
+
     public void setScores(int scores) {
         this.scores = scores;
     }
+
     public Boolean getResult() {
         return result;
     }
+
     public void setResult(Boolean result) {
         this.result = result;
     }
 
     public ScoreManager() {
-    } 
+    }
 
-    static boolean checkTheScore( int scores){
-        boolean result = false; 
+    static boolean checkTheScore(int scores) {
+        boolean result = false;
         if (scores < 0) {
             throw new IllegalArgumentException("Score cannot be negative.");
         }
 
         if (scores >= 6) {
-            result = true; 
+            result = true;
         }
-    
-        return result; 
+
+        return result;
 
     }
 
-    
 }
--- src/test/java/ca/vanier/ScoreManagerTest.java
+++ src/test/java/ca/vanier/ScoreManagerTest.java
@@ -6,16 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ScoreManagerTest {
     @Test
     public void TestPassingScore() {
-
+        int score = 7;
+        assertTrue(ScoreManager.checkTheScore(score));
     }
 
     @Test
     public void TestExactPassingScore() {
-
+        int score = 6;
+        assertTrue(ScoreManager.checkTheScore(score));
     }
 
     @Test
     public void TestFaillingScore() {
+        int score = 5;
+        assertFalse(ScoreManager.checkTheScore(score));
+    }
 
+    @Test
+    public void TestNegativeScore() {
+        int score = -5;
+        assertThrows(IllegalArgumentException.class, () -> {
+            ScoreManager.checkTheScore(score);
+        });
     }
 }
