d9216e612a8e587f8a8b45c45d7142862bf10855
Kevin Britten <kevinjbritten@gmail.com>
2024-12-03 17:44:29 -0500

    add placeholder tests

---
+++ src/test/java/ca/vanier/ScoreManagerTest.java
@@ -0,0 +1,21 @@
+package ca.vanier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreManagerTest {
+    @Test
+    public void TestPassingScore() {
+
+    }
+
+    @Test
+    public void TestExactPassingScore() {
+
+    }
+
+    @Test
+    public void TestFaillingScore() {
+
+    }
+}
