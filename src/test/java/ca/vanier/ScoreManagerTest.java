b15c3d9b80c4477dbfb5f0c7d1d7aa45b2a5c808
Kevin Britten <kevinjbritten@gmail.com>
2024-12-03 19:00:45 -0500

    add dependancies for and implement parameterized tests

--- src/test/java/ca/vanier/ScoreManagerTest.java
+++ src/test/java/ca/vanier/ScoreManagerTest.java
@@ -3,22 +3,20 @@ package ca.vanier;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class ScoreManagerTest {
-    @Test
-    public void TestPassingScore() {
-        int score = 7;
+
+    @ParameterizedTest
+    @ValueSource(ints = { 6, 7 })
+    public void TestPassingScore(int score) {
         assertTrue(ScoreManager.checkTheScore(score));
     }
 
-    @Test
-    public void TestExactPassingScore() {
-        int score = 6;
-        assertTrue(ScoreManager.checkTheScore(score));
-    }
-
-    @Test
-    public void TestFaillingScore() {
-        int score = 5;
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5 })
+    public void TestFaillingScore(int score) {
         assertFalse(ScoreManager.checkTheScore(score));
     }
 
